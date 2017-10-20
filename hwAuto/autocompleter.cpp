fa92d314001a5b1cd515ee14f5e7afccae17954f
josepadilla01 <wichopadillan@hotmail.com>
2017-10-20 18:28:19 -0500

    update

--- hwAuto/autocompleter.cpp
+++ hwAuto/autocompleter.cpp
@@ -57,6 +57,32 @@ int Autocompleter::size_recurse(Node *root)
 	return (1 + size_recurse(root->left) + size_recurse(root->right));
 }
 
+int Autocompleter::completion_count(string x)
+{
+	return completion_count_recurse(x, root);
+}
+
+int Autocompleter::completion_count_recurse(string x, Node* root)
+{
+	if (root == nullptr)
+	{
+		return 0;
+	}
+
+	int sLength = x.size();
+
+	if (root->e.s.substr(0, sLength) == x)
+	{
+		return 1 + completion_count_recurse(x, root->left);
+	}
+	else if (root->e.s.substr(0, sLength) > x)
+	{
+		return 0 + completion_count_recurse(x, root->left);
+	}
+	else
+		completion_count_recurse(x, root->right);
+}
+
 //void Autocompleter::completions(string x, vector<string> &C)
 //{
 //	vector<Entry> E;
@@ -66,18 +92,24 @@ int Autocompleter::size_recurse(Node *root)
 //		C.push_back(E[i].s);
 //	}
 //}
-//
+////
 //void Autocompleter::completions_recurse(string x, Node* root, vector<Entry> &E)
 //{
 //	
 //	if (root == nullptr)
 //		return;
 //	
+//
+//	//Get the substring from the string in the entry
+//	//that equals in size of input string
+//	int sLength = x.size();
+//
 //	//recurse
-//	if (root->e.s <= x)
+//	//if input string is equal to the substring of the entry
+//	if (root->e.s.substr(0, sLength) <= x)
 //	{
 //		
-//		E.push_back(root->e);
+//		E.push_back(root->e); //E.push_back(completions_recurse(x, root->left, E);????????
 //		completions_recurse(x, root->left, E);
 //		cout << root->left->e.freq;
 //	}
